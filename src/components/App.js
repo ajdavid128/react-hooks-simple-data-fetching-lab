775fd72e257adb2dec2463dc534098de012a95f8
Aaron David <ajdavid128@gmail.com>
2022-11-07 21:51:09 -0600

    commit tree e027814efff878302e98032e7e35f4908e20c227

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
 // create your App component here
+
+import {useEffect, useState} from "react"
+
+function App() {
+
+    const [fetching, setFetching] = useState(null)
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(r => r.json())
+        .then(data => setFetching(data))
+      }, []);
+
+      if (!fetching) {
+        return( <p>Loading...</p>)
+      }
+
+    return (
+        <>
+            <h1>Unsolicited Dog Pics</h1>
+            <img src={fetching.message} alt="A Random Dog" />
+        </>
+    )
+}
+
+export default App;
